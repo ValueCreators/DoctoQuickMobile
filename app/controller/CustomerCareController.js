bae295fa5e41fbc998941d8610d5dccaec385a38
VALUE CREATORS <thiyagarajan.jaganathan@sourcebits.com>
2015-07-21 23:41:12 +0530

    merge conflict -thiyagu

--- app/controller/CustomerCareController.js
+++ app/controller/CustomerCareController.js
@@ -3,10 +3,13 @@ Ext.define('DoctorQuickMobile.controller.CustomerCareController',{
 	extend: 'Ext.app.Controller',    
 	config:{
 				refs:{
-							
+					customerCareView : 'customerCareView',
+					mainPanelContainer : 'doctor_mainPanel panel#_mainPanelContainer',
+					mainPanelToolbarId : 'doctor_mainPanel toolbar#mainPanelToolbarId',
+					gotoDashBoardBtn : 'doctor_mainPanel toolbar button#gotoDashBoardBtn'					
 				},
 				control : {
-							
+					customerCareView : { activate : 'customerCareActivate', deactivate : 'customerCareDeactivate', scope : this }	
 				}
 	},
 	
@@ -15,7 +18,19 @@ Ext.define('DoctorQuickMobile.controller.CustomerCareController',{
 	},
 	
 	showCustomerCareView : function(){
-		alert('customer care view');
-		this.customerView = Ext.Viewport.add(Ext.create('DoctorQuickMobile.view.common.CustomerCareView'));
+		//alert('customer care view');
+		//this.customerView = Ext.Viewport.add(Ext.create('DoctorQuickMobile.view.common.CustomerCareView'));
+		this.getMainPanelContainer().setActiveItem('customerCareView');
+       	this.getMainPanelToolbarId().setTitle("Customer Care");
+	},
+
+	customerCareActivate : function () {
+		//this.getDocProfileEditOrSubmitBtn().show();
+		this.getGotoDashBoardBtn().show();
+	},
+
+	customerCareDeactivate : function () {
+		//this.getDocProfileEditOrSubmitBtn().hide();
+		this.getGotoDashBoardBtn().hide();	
 	}
 });
