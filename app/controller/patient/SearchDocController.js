f3c4654e7c4f1d26c40aa7dd918b4e72106a9b90
valuecreators3@gmail.com <vinay.rgit@gmail.com>
2015-07-25 10:13:50 +0530

    update

---
+++ app/controller/patient/SearchDocController.js
@@ -0,0 +1,20 @@
+Ext.define('DoctorQuickMobile.controller.patient.SearchDocController',{
+				
+	extend: 'Ext.app.Controller',    
+	config:{
+				refs:{
+							
+				},
+				control : {
+							
+				}
+	},
+	
+	init : function(){
+		
+	},
+	
+	showSearchDocView : function(){
+		
+	}
+});
